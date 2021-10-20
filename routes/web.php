91b940b2f607b12db0598b18f124ab1300564477
toobamalaika <toobachuhan92@gmail.com>
2021-10-20 10:38:24 +0500

    add pizza form UI

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// For Pizza
+Route::get('/pizza', [App\Http\Controllers\PizzaController::class, 'index'])->name('pizza.index');
