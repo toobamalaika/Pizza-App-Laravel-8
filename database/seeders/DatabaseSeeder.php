2af75c7b3a0d6f25a6cfdc57f9073e274ffac86b
Tooba Malaika <toobachuhan92@gmail.com>
2021-11-05 12:52:44 +0500

    Add middleware for for routes and make seeder for super user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('password');
+        $user->is_admin = 1;
+        $user->save();
     }
 }
